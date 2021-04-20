731fcf571deeb22d2d26e8d66d8e5dcee9c3903b
DESKTOP-EJ4BP01\PSH <khansol1004@gmail.com>
2021-04-20 17:25:17 +0900

    다 빨강색이야...

--- store/MysqlConnector.js
+++ store/MysqlConnector.js
@@ -1,7 +1,7 @@
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
-  port: 3306,
+  port: 3307,
   user     : 'sanghyunpark',
   password : 'qkrtkd12',
   database : 'accountbook'
